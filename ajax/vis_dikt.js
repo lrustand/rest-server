43eda59630896771463632f4a82ac20ca27a5fdb
Lars Rustand <rustand.lars@gmail.com>
2020-03-13 19:14:40 +0000

    vis_dikt

---
+++ ajax/vis_dikt.js
@@ -0,0 +1,33 @@
+
+const queryString = new URLSearchParams(window.location.search);
+const diktid = queryString.get("diktid")
+
+// Viser dikt spesifisert i query string (?diktid=)
+if (diktid != null) {
+	getUrl("/diktsamling/dikt/"+diktid, function(xhttp) {
+		JSON.parse(xhttp.responseText).forEach(function (dikt, index) {
+			console.log(dikt)
+			var out = "<div class='dikt'>"
+			out += `<h3><a href=vis_dikt.html?diktid=${dikt.diktid}>Dikt #${dikt.diktid}</a></h3>`
+			out += dikt.dikt
+			out += "<br>"
+			out += "<p><i>- "+dikt.fornavn+" "+dikt.etternavn+"<i><p>"
+			out += "</div>"
+			document.getElementsByClassName("main")[0].innerHTML += out
+		})
+	})
+}
+
+// Liste over alle dikt
+else {
+	getUrl("/diktsamling/dikt/", function(xhttp) {
+		const valgte_dikt = JSON.parse(xhttp.responseText)
+		valgte_dikt.forEach(function (dikt, index) {
+			var row = "<div class='dikt'>"
+			row += `<h3><a href=vis_dikt.html?diktid=${dikt.diktid}>Dikt #${dikt.diktid}</a></h3>`
+			row += "Skrevet av " + dikt.fornavn + " " + dikt.etternavn
+			row += "</div>"
+			document.getElementsByClassName("main")[0].innerHTML += row
+		})
+	})
+}
