d7d1e9830d7956575d9c8cce511f64dc2fa8e3fb
Lars Rustand <rustand.lars@gmail.com>
2020-03-13 10:08:14 +0000

    Logger inn i rest api

--- ajax/main.js
+++ ajax/main.js
@@ -48,4 +48,16 @@ getUrl("/diktsamling/sesjon", function(xhttp) {
 	}
 })
 
+// Send inn innloggingsdetaljer til rest api
+function login() {
+	var data = {}
 
+	data.username = document.getElementById("username").value
+	data.password = document.getElementById("password").value
+
+	postUrl("/diktsamling/sesjon", data, function(xhttp) {
+		if (xhttp.status == 200) {
+		}
+	})
+	return false
+}
