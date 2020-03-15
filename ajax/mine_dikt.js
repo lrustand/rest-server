7d9d78f4501ce7ce534b8fe2ec0be74b92cea7af
Lars Rustand <rustand.lars@gmail.com>
2020-03-15 10:56:43 +0000

    Slett dikt

--- ajax/mine_dikt.js
+++ ajax/mine_dikt.js
@@ -2,10 +2,25 @@
 getUrl("/diktsamling/bruker", function(xhttp) {
 	var mine_dikt = JSON.parse(xhttp.responseText)
 	mine_dikt.forEach(function (dikt, index) {
-		var row = "<div class='dikt'>"
+		var row = `<div class="dikt" id="dikt_${dikt.diktid}">`
 		row += `<h3><a href=vis_dikt.html?diktid=${dikt.diktid}>Dikt #${dikt.diktid}</a></h3>`
 		row += dikt.dikt
+		row += "<br><br>"
+		row += `<button onclick="slettDikt(${dikt.diktid});">Slett</button>`
 		row += "</div>"
 		document.getElementsByClassName("main")[0].innerHTML += row
 	})
 })
+
+
+function slettDikt(diktid) {
+	var ans = confirm(`Er du sikker på at du vil slette dikt ${diktid}?`)
+	if (ans) {
+		AJAXRequest("DELETE", `/diktsamling/dikt/${diktid}`, null, function(xhttp) {
+			if (xhttp.status == 200) {
+				var dikt = document.getElementById(`dikt_${diktid}`)
+				dikt.parentElement.removeChild(dikt)
+			}
+		})
+	}
+}
