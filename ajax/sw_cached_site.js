8f50cc51baee5b83acda9c957afb54f15612d6b3
Nelde95 <Nelde95@yahoo.com>
2020-03-16 04:43:50 +0100

    Cacher nå sider etterhvært som de blir åpnet

--- ajax/sw_cached_site.js
+++ ajax/sw_cached_site.js
@@ -1,29 +1,8 @@
-const cacheName = 'v1'
-
-const cacheAssets = [
-	'/ajax/index.html',
-	'/ajax/style.css',
-	'/diktsamling/sesjon',
-	'/ajax/vis_dikt.html',
-	'/ajax/login.html',
-	'/diktsamling/dikt/',
-	'/ajax/main.js',
-	'/ajax/vis_dikt.js'
-]
+const cacheName = 'v2'
 
 // Call Install Event
 self.addEventListener('install', (e) => {
 	console.log('Service Worker: Installed')
-
-	e.waitUntil(
-		caches
-			.open(cacheName)
-			.then(cache => {
-				console.log('Service Worker: Caching Files')
-				cache.addAll(cacheAssets)
-			})
-			.then(() => self.skipWaiting())
-	)
 })
 
 // Call Acitvate Event
@@ -47,6 +26,19 @@ self.addEventListener('activate', (e) => {
 // Call Fetch Event
 self.addEventListener('fetch', e => {
 	console.log('Service Worker: Fetching')
-	e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
+	e.respondWith(
+		fetch(e.request)
+			.then(res => {
+				// Make clone of response
+				const resClone = res.clone()
+				// Open cache
+				caches.open(cacheName).then(cache => {
+					// Add response to cache
+					cache.put(e.request, resClone)
+				})
+				return res
+			})
+			.catch(err => caches.match(e.request).then(res => res))
+	)
 })
 
