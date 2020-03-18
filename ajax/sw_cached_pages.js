7c145af40f0b8afbb5bb36b76b5c60f5702dcc33
Lars Rustand <rustand.lars@gmail.com>
2020-03-18 23:11:25 +0000

    Oppdaterer cachet diktliste med entries basert på dikt hentet fra diktsider

--- ajax/sw_cached_pages.js
+++ ajax/sw_cached_pages.js
@@ -60,6 +60,34 @@ self.addEventListener('activate', (e) => {
 })
 
 
+// Oppdaterer cachet diktliste når bruker laster inn en diktside
+function oppdaterDikt(dikt) {
+	caches.match('/diktsamling/dikt/') // Henter diktliste fra cache
+		.then(res =>  res.json())
+		.then(alle_dikt => {
+			var alle_dikt = [].concat(alle_dikt) // Lager skrivbar kopi
+			var found = false
+			for(var i = 0; i < alle_dikt.length; i++) {
+				var d = alle_dikt[i]
+				// Oppdaterer eksisterende entry
+				if (d.diktid == dikt.diktid) {
+					alle_dikt[i] = dikt
+					found = true
+					break
+				}
+			}
+			// Legger til diktet hvis det ikke er i lista allerede
+			if (!found) {
+				alle_dikt.push(dikt)
+			}
+			// Legger den oppdaterte diktlisten i cache
+			console.log("Oppdaterer dikt i diktliste")
+			caches.open(cacheName).then(cache => {
+				cache.put("/diktsamling/dikt/", new Response(JSON.stringify(alle_dikt)))
+			})
+		})
+}
+
 function doFetch(e) {
 	return fetch(e.request)
 		.then(res => {
@@ -70,6 +98,14 @@ function doFetch(e) {
 				// Add response to cache
 				cache.put(e.request, resClone)
 			})
+			if (e.request.url.startsWith('http://127.0.0.1:3000/diktsamling/dikt/')
+				&& e.request.url != 'http://127.0.0.1:3000/diktsamling/dikt/') {
+				res.clone().json().then(dikt => {
+					if (dikt != null) {
+						oppdaterDikt(dikt[0])
+					}
+				})
+			}
 			return res
 		})
 		.catch(err => null)
@@ -94,7 +130,7 @@ function doDynamic(e) {
 				}
 			}
 
-			// Selve html-filene er like uavhengig av query string
+			// Selve html-filene er like, uavhengig av query string
 			if (url.startsWith('http://127.0.0.1:3000/ajax/vis_dikt.html?')) {
 				return caches.match('/ajax/vis_dikt.html')
 			}
@@ -109,24 +145,25 @@ function doDynamic(e) {
 // Call Fetch Event
 self.addEventListener('fetch', (e) => {
 	console.log(`Service Worker: Client requested ${e.request.url}`)
-	e.respondWith(async function(){
-		const fetched = await doFetch(e)
-		if (fetched) {
-			console.log("Responding with fetched content")
-			return fetched
-		}
+	const fetched = doFetch(e).then(fetched => fetched)
+	if (fetched) {
+		console.log("Responding with fetched content")
+		e.respondWith(fetched)
+		return
+	}
 
-		const dynamic = await doDynamic(e)
-		if (dynamic) {
-			console.log("Responding with dynamic content")
-			return dynamic
-		}
+	const dynamic = doDynamic(e).then(dynamic => dynamic)
+	if (dynamic) {
+		console.log("Responding with dynamic content")
+		e.respondWith(dynamic)
+		return
+	}
 
-		const cached = caches.match(e.request).then(res => res)
-		if (cached) {
-			console.log("Responding with cached content")
-			return cached
-		}
+	const cached = caches.match(e.request).then(res => res)
+	if (cached) {
+		console.log("Responding with cached content")
+		e.respondWith(cached)
+		return
+	}
 
-	}())
 })
